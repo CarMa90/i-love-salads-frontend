b526ba0f987d5a2d32798e19dc5e7cf4125f98c4
CarMa90 <el.matteotti@gmail.com>
2026-09-02 06:40:04 -0700

    update image routes at constants

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -10,7 +10,7 @@ export const PRODUCTS = [
     name: "E. Pollo Manzana",
     description:
       "Cama de lechuga, vinagreta de balsámico, mezcla de pollo con manzana y chile jalapeño, uvas, queso feta y almendras tostadas.",
-    image: "../images/pollo-manzana.jpg",
+    image: `${import.meta.env.BASE_URL}images/pollo-manzana.jpg`,
     price: 149,
     type: "Ensaladas",
     _id: "650f1f1e1c4d7b001c8e9f1a",
@@ -19,7 +19,7 @@ export const PRODUCTS = [
     name: "E. Mexicana",
     description:
       "Cama de lechuga, aderezo de cilantro, arrachera, tomate, granos de elote amarillo, frijol negro, aguacate, queso fresco y tiritas de tortilla.",
-    image: "../images/pollo-manzana.jpg",
+    image: `${import.meta.env.BASE_URL}images/pollo-manzana.jpg`,
     price: 149,
     type: "Ensaladas",
     _id: "650f1f1e1c4d7b001c8e9f1b",
@@ -28,7 +28,7 @@ export const PRODUCTS = [
     name: "E. Pasta al Pesto",
     description:
       "Cama de pasta fusilli, aderezo pesto, pechuga de pollo a la plancha, tomate, pimiento amarillo, aceitunas negras y queso parmesano (no contiene lechuga y se sirve fría).",
-    image: "../images/pollo-manzana.jpg",
+    image: `${import.meta.env.BASE_URL}images/pollo-manzana.jpg`,
     price: 149,
     type: "Ensaladas",
     _id: "64f0b2a1e4b01a2c3d4e5f6a",
@@ -37,7 +37,7 @@ export const PRODUCTS = [
     name: "E. Griega",
     description:
       "Cama de lechuga, vinagreta de balsámico, pechuga de pollo a la plancha, tomate, aceitunas negras, pepino, pimento amarillo y queso feta.",
-    image: "../images/pollo-manzana.jpg",
+    image: `${import.meta.env.BASE_URL}images/pollo-manzana.jpg`,
     price: 141,
     type: "Ensaladas",
     _id: "64f0b2a1e4b01a2c3d4e5f6b",
@@ -46,7 +46,7 @@ export const PRODUCTS = [
     name: "E. Oriental",
     description:
       "Cama de lechuga, aderezo oriental, pechuga de pollo a la plancha, zanahoria, repollo morado y crujientes de won ton",
-    image: "../images/pollo-manzana.jpg",
+    image: `${import.meta.env.BASE_URL}images/pollo-manzana.jpg`,
     price: 141,
     type: "Ensaladas",
     _id: "64f0b2a1e4b01a2c3d4e5f6c",
@@ -55,7 +55,7 @@ export const PRODUCTS = [
     name: "B. Pollo con Chipotle",
     description:
       "Pechuga de pollo a la plancha con crema de chipotle, mayonesa, queso monterrey, tomate, lechuga y aguacate.",
-    image: "../images/pollo-chipotle.jpg",
+    image: `${import.meta.env.BASE_URL}images/pollo-chipotle.jpg`,
     price: 144,
     type: "Baguettes",
     _id: "650f1f1e1c4d7b001c8e9f1c",
@@ -64,7 +64,7 @@ export const PRODUCTS = [
     name: "B. Pollo Poblano",
     description:
       "Pechuga de pollo a la plancha con crema de chile poblano, mayonesa, queso monterrey y granos de elote amarillo.",
-    image: "../images/pollo-chipotle.jpg",
+    image: `${import.meta.env.BASE_URL}images/pollo-chipotle.jpg`,
     price: 144,
     type: "Baguettes",
     _id: "650f1f1e1c4d7b001c8e9f1d",
@@ -73,7 +73,7 @@ export const PRODUCTS = [
     name: "S. de Tortilla",
     description:
       "Base de tomate, queso fresco, aguacate, tiritas de tortilla y crema ácida.",
-    image: "../images/sopa-tortilla.jpg",
+    image: `${import.meta.env.BASE_URL}images/sopa-tortilla.jpg`,
     price: 61,
     type: "Sopas",
     _id: "507f1f77bcf86cd799439011",
@@ -81,7 +81,7 @@ export const PRODUCTS = [
   {
     name: "Caldo de Res",
     description: "Pecho de res, elote, chile poblano, zanahoria, papa y arroz.",
-    image: "../images/sopa-tortilla.jpg",
+    image: `${import.meta.env.BASE_URL}images/sopa-tortilla.jpg`,
     price: 61,
     type: "Sopas",
     _id: "64a2b2f8e4b09a123456789a",
@@ -90,7 +90,7 @@ export const PRODUCTS = [
     name: "Agua fresca",
     description:
       "Botella de medio litro de agua fresca natural con azúcar regular.",
-    image: "../images/agua-fresca.jpg",
+    image: `${import.meta.env.BASE_URL}images/agua-fresca.jpg`,
     price: 36,
     type: "Bebidas",
     _id: "5f50c31b83f3a421b8f01234",
@@ -98,7 +98,7 @@ export const PRODUCTS = [
   {
     name: "Refrescos",
     description: "Refresco en lata de 355 ml de la marca coca cola",
-    image: "../images/agua-fresca.jpg",
+    image: `${import.meta.env.BASE_URL}images/agua-fresca.jpg`,
     price: 29,
     type: "Bebidas",
     _id: "60c72b2f9b1d8b0015d8123f",
