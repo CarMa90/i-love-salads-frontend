d959020515cbd2b121d6fbc52f591048920545fc
CarMa90 <el.matteotti@gmail.com>
2026-09-13 12:34:09 -0700

    update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { authorize } from "../../utils/auth";
 import { useContext } from "react";
-import { ProductsContext } from "../../contexts/ProductsContext";
 import { UserContext } from "../../contexts/UserContext";
 import InfoTooltip from "../InfoTooltip/InfoTooltip";
 import { setToken } from "../../utils/token";
@@ -17,8 +16,6 @@ function Login() {
     setIsLoggedIn,
     setCurrentUser,
   } = useContext(UserContext);
-
-  const { setLoader } = useContext(ProductsContext);
 
   const [data, setData] = useState({
     email: "",
